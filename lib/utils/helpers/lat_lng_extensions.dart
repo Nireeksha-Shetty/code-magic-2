9462d4f194c218f03e902256286102ee68e2367c
Shetty <nshetty@mtm-inc.net>
2024-05-08 12:14:28 +0530

    a

--- lib/utils/helpers/lat_lng_extensions.dart
+++ lib/utils/helpers/lat_lng_extensions.dart
@@ -1,32 +1,32 @@
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:maps_toolkit/maps_toolkit.dart' as mtk;
-import 'package:latlong2/latlong.dart' as lat2;
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:maps_toolkit/maps_toolkit.dart' as mtk;
+// import 'package:latlong2/latlong.dart' as lat2;
 
-extension LatUtils on LatLng {
-  lat2.LatLng toLat2() {
-    lat2.LatLng l = lat2.LatLng(this.latitude, this.longitude);
-    return l;
-  }
+// extension LatUtils on LatLng {
+//   lat2.LatLng toLat2() {
+//     lat2.LatLng l = lat2.LatLng(this.latitude, this.longitude);
+//     return l;
+//   }
 
-  mtk.LatLng toMtk() {
-    mtk.LatLng l = mtk.LatLng(this.latitude, this.longitude);
+//   mtk.LatLng toMtk() {
+//     mtk.LatLng l = mtk.LatLng(this.latitude, this.longitude);
 
-    return l;
-  }
-}
+//     return l;
+//   }
+// }
 
-extension MtkUtils on mtk.LatLng {
-  LatLng toLatLng() {
-    LatLng l = LatLng(this.latitude, this.longitude);
+// extension MtkUtils on mtk.LatLng {
+//   LatLng toLatLng() {
+//     LatLng l = LatLng(this.latitude, this.longitude);
 
-    return l;
-  }
-}
+//     return l;
+//   }
+// }
 
-extension Lat2Utils on lat2.LatLng {
-  LatLng toLatLng() {
-    LatLng l = LatLng(this.latitude, this.longitude);
+// extension Lat2Utils on lat2.LatLng {
+//   LatLng toLatLng() {
+//     LatLng l = LatLng(this.latitude, this.longitude);
 
-    return l;
-  }
-}
+//     return l;
+//   }
+// }
